fd73be0b3fb18f68e455d61a7065956a957af191
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2022-10-19 03:49:42 -0500

    Update 3_run_regem.sh

--- REGEM-simulations/3_run_regem.sh
+++ REGEM-simulations/3_run_regem.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sets=("sexE" "bmiE" "sexI_bmiE" "sexE_bmiE") 
-
 #WHR phenotype, GxSex + GxBMI
 dx run run_regem --instance-type mem1_ssd1_v2_x8 --name "run_REGEM" --destination "/GEM-REGEM-Comparisons/Results/REGEM/sexE/results" -iinputfile="/GEM-REGEM-Comparisons/Results/GEMv1.4.1/sexE_bmiI/combined/gem_sexE_bmiI_combined.out" -iexposure_names="sex" -ioutput_style="full" -icpu=1 -idisk=10 -imemory=3 -ipreemptible=0 -y
 
